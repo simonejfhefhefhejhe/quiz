4a0c764b56f3b48065f4167b982c310d2ea8bfce
SEMEN <semensysoevsteamacount@gmail.com>
2025-05-31 08:45:53 -0400

    start of the progect

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-questions = {""
+import time
+questions = {
              "сколько планет в солчной системе?": "8",
                                                   "столица Франии?": "Париж"}
 score = 0
 
 for q, a in questions.items():
+    start_time = time.time()
     user_answer = input(q + " ")
+    end_time = time.time()
+    if round(end_time - start_time, 1 ) > 5:
+        print('вы не уложились в таймер 5 секнд')
+        user_answer = 'wrong'
+    print (f"врем ответа : {round(end_time - start_time, 1)} сек ")
     if user_answer.lower() ==a.lower():
         print("правильно ")
         score += 1
@@ -13,3 +20,6 @@ for q, a in questions.items():
         print("неверно")
 
 print (f"ваш результат: {score} из {len(questions)}")
+
+
+
